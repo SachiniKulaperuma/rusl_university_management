50c2733c0400abe70bd5a47a73f6b8e66ef88084
GayashanDeshapriya <gayashan.deshapriya.dev@gmail.com>
2026-07-12 22:21:51 +0530

    Wrap header component in a fragment for improved structure and readability

--- app/components/layout/Header/Header.tsx
+++ app/components/layout/Header/Header.tsx
@@ -5,9 +5,11 @@ import Navbar from "./Navbar";
 export default function Header() {
     return (
         <>
+        <header className="sticky top-0 z-50 bg-red-900 shadow-lg" id="site-header">
             <HeaderTop />
             <HeaderInfo />
             <Navbar />
+            </header>
         </>
     );
 }
